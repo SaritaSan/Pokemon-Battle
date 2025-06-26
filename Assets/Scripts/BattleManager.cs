23f7b564f7898c38c11e416aba8615c58ca325fe
SaritaSan <156473953+SaritaSan@users.noreply.github.com>
2025-06-25 20:06:34 -0600

    Cuenta regresiva | música de fondo
     imagenes, audios y marco

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -8,6 +8,8 @@ public class BattleManager : MonoBehaviour
     [SerializeField]
     private int _numberOfFighters = 2;
     [SerializeField]
+    private UnityEvent _onBattleStopped;
+    [SerializeField]
     private UnityEvent _onFightersReady;
     [SerializeField]
     private UnityEvent _onBattleFinished;
@@ -28,10 +30,14 @@ public class BattleManager : MonoBehaviour
     public void RemoveFighter(Fighter fighter)
     {
         _fighters.Remove(fighter);
-        if (_battleCoroutine != null)
+        if (_fighters.Count < 2)
         {
-            StopCoroutine(_battleCoroutine);
-            _battleCoroutine = null;
+            if (_battleCoroutine != null)
+            {
+                StopCoroutine(_battleCoroutine);
+                _battleCoroutine = null;
+            }
+            _onBattleStopped?.Invoke();
         }
     }
     private void CheckFighters()
@@ -41,7 +47,7 @@ public class BattleManager : MonoBehaviour
             return;
         }
         _onFightersReady?.Invoke();
-        StartBattle();
+        _onBattleStarted?.Invoke();
     }
     public void StartBattle()
     {
@@ -54,7 +60,6 @@ public class BattleManager : MonoBehaviour
     }
     private IEnumerator BattleCoroutine()
     {
-        _onBattleStarted?.Invoke();
         while (_fighters.Count > 1)
         {
             Fighter attacker = _fighters[Random.Range(0, _fighters.Count)];
@@ -80,7 +85,7 @@ public class BattleManager : MonoBehaviour
             defender.Health.TakeDamage(_damageTarget);
             if (defender.Health.CurrentHealth <= 0)
             {
-                _fighters.Remove(defender);
+                RemoveFighter(defender);
                 
             }
             yield return new WaitForSeconds(1f);
